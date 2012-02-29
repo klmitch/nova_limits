c8a25824e4702591fd3e8da41733003c36f75edf
Kevin L. Mitchell <kevin.mitchell@rackspace.com>
2012-02-29 13:29:04 -0600

    Add licensing and copyrights

--- setup.py
+++ setup.py
@@ -12,8 +12,17 @@ setup(
     author='Kevin L. Mitchell',
     author_email='kevin.mitchell@rackspace.com',
     description="Nova-specific rate-limit class for turnstile",
-    license='',
+    license='Apache License (2.0)',
     py_modules=['nova_limits'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Paste',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware',
+        ],
     url='https://github.com/klmitch/nova_limits',
     long_description=read('README.rst'),
     install_requires=[
